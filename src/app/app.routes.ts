567da9769dae8bf96d2b50eea109813efda3b1df
STEPH <s.desray@gmail.com>
2025-09-30 10:48:25 +0200

    add datas and final fonctions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { Artisans } from './pages/artisans/artisans';
+import { ArtisanDetailComponent } from './pages/artisan-detail/artisan-detail.component';
+import { LegalComponent } from './pages/legal/legal';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Route pour la page d'accueil
+  { path: '', component: HomeComponent },
+
+  // Route pour la liste des artisans
+  { path: 'artisans', component: Artisans },
+
+  { path: 'artisans/:category', component: Artisans },
+
+  // Route pour la page de détail d'un artisan (avec un paramètre 'id')
+  { path: 'artisan/:id', component: ArtisanDetailComponent },
+
+  // Route pour la page des mentions légales
+  { path: 'legal', component: LegalComponent },
+
+  // Route joker pour gérer les pages non trouvées
+  { path: '**', component: PageNotFoundComponent }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('projet-app-angular-f');
+  protected readonly title = signal('projet-app-angular-1');
 }
